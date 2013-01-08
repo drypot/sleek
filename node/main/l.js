71957008329592c63e28f17176bcd17549d90c75
drypot <drypot@gmail.com>
2013-01-09 06:23:27 +0900

    added John Resig's JavaScript Inheritance.

--- node/main/l.js
+++ node/main/l.js
@@ -136,6 +136,49 @@ l.init(function () {
 
 l.init(function () {
 
+	// http://ejohn.org/blog/simple-javascript-inheritance/
+
+	var initializing = false,
+		superPattern = /xyz/.test(function () { xyz; }) ? /\b_super\b/ : /.*/;
+
+	Object.subClass = function (properties) {
+		var _super = this.prototype;
+
+		initializing = true;
+		var proto = new this();
+		initializing = false;
+
+		for (var name in properties) {
+			proto[name] = typeof properties[name] == "function" &&
+				typeof _super[name] == "function" &&
+				superPattern.test(properties[name]) ?
+				(function (name, fn) {
+					return function () {
+						var tmp = this._super;
+						this._super = _super[name];
+						var ret = fn.apply(this, arguments);
+						this._super = tmp;
+						return ret;
+					};
+				})(name, properties[name]) :
+				properties[name];
+		}
+
+		function Class() {
+			// All construction is actually done in the init method
+			if (!initializing && this.init)
+				this.init.apply(this, arguments);
+		}
+
+		Class.prototype = proto;
+		Class.constructor = Class;
+		Class.subClass = arguments.callee;
+		return Class;
+	}
+});
+
+l.init(function () {
+
 	// http://stove99.tistory.com/46
 
 	Date.prototype.format = function(f) {
