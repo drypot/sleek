fe232e4ceaf85b46553e7d86ea080065f50f9025
drypot <drypot@gmail.com>
2013-04-02 16:54:29 +0900

    used should.not.exist(err);

--- server/test/mongo-test.js
+++ server/test/mongo-test.js
@@ -32,23 +32,17 @@ describe('post collection', function () {
 	});
 	it('should be empty', function (next) {
 		mongo.postCol.count(function (err, count) {
-			if (err) {
-				next(err);
-			} else {
-				count.should.equal(0);
-				next();
-			}
+			should.not.exist(err);
+			count.should.equal(0);
+			next();
 		})
 	});
 	it('should have two indexes', function (next) {
 		mongo.postCol.indexes(function (err, index) {
-			if (err) {
-				next(err);
-			} else {
-				index.should.be.instanceof(Array);
-				index.should.be.length(2);
-				next();
-			}
+			should.not.exist(err);
+			index.should.be.instanceof(Array);
+			index.should.be.length(2);
+			next();
 		});
 	});
 	it('can make serialized ids', function () {
@@ -86,23 +80,17 @@ describe('post collection', function () {
 	});
 	it('can count records', function (next) {
 		mongo.postCol.count(function (err, count) {
-			if (err) {
-				next(err);
-			} else {
-				count.should.equal(5);
-				next();
-			}
+			should.not.exist(err);
+			count.should.equal(5);
+			next();
 		});
 	});
 	it('can find post by id', function (next) {
 		mongo.findPostById(ppost._id, function (err, post) {
-			if (err) {
-				next(err);
-			} else {
-				post._id.should.equal(ppost._id);
-				post.text.should.equal(ppost.text);
-				next();
-			}
+			should.not.exist(err);
+			post._id.should.equal(ppost._id);
+			post.text.should.equal(ppost.text);
+			next();
 		});
 	});
 	it('can update', function (next) {
@@ -110,44 +98,32 @@ describe('post collection', function () {
 		ppost.hxit = 17;
 		mongo.updatePost(ppost, function (err) {
 			mongo.findPostById(ppost._id, function (err, post) {
-				if (err) {
-					next();
-				} else {
-					post.should.eql(ppost);
-					next();
-				}
+				should.not.exist(err);
+				post.should.eql(ppost);
+				next();
 			});
 		});
 	});
 	it('can find posts by thread id', function (next) {
 		mongo.findPostByThread(1000, function (err, post) {
-			if (err) {
-				next();
-			} else {
-				post.should.length(3);
-				next();
-			}
+			should.not.exist(err);
+			post.should.length(3);
+			next();
 		})
 	});
 	it('can find posts by thread id, 2', function (next) {
 		mongo.findPostByThread(1010, function (err, post) {
-			if (err) {
-				next();
-			} else {
-				post.should.length(2);
-				next();
-			}
+			should.not.exist(err);
+			post.should.length(2);
+			next();
 		})
 	});
 	it('can find posts as sorted', function (next) {
 		mongo.findPostByThread(1000, function (err, post) {
-			if (err) {
-				next();
-			} else {
-				post[0].created.should.below(post[1].created);
-				post[1].created.should.below(post[2].created);
-				next();
-			}
+			should.not.exist(err);
+			post[0].created.should.below(post[1].created);
+			post[1].created.should.below(post[2].created);
+			next();
 		})
 	});
 });
@@ -158,23 +134,17 @@ describe('thread collection', function () {
 	});
 	it('should be empty', function (next) {
 		mongo.threadCol.count(function (err, count) {
-			if (err) {
-				next(err);
-			} else {
-				count.should.equal(0);
-				next(err);
-			}
+			should.not.exist(err);
+			count.should.equal(0);
+			next(err);
 		})
 	});
 	it('should have one index', function (next) {
 		mongo.threadCol.indexes(function (err, indexList) {
-			if (err) {
-				next(err);
-			} else {
-				indexList.should.be.instanceof(Array);
-				indexList.should.be.length(3);
-				next(err);
-			}
+			should.not.exist(err);
+			indexList.should.be.instanceof(Array);
+			indexList.should.be.length(3);
+			next(err);
 		});
 	});
 	it('can make serialized ids', function () {
@@ -232,23 +202,17 @@ describe('thread collection', function () {
 	});
 	it('can count records', function (next) {
 		mongo.threadCol.count(function (err, count) {
-			if (err) {
-				next(err);
-			} else {
-				count.should.equal(10);
-				next();
-			}
+			should.not.exist(err);
+			count.should.equal(10);
+			next();
 		});
 	});
 	it('can find by id', function (next) {
 		mongo.findThreadById(pthread._id, function (err, thread) {
-			if (err) {
-				next(err);
-			} else {
-				thread._id.should.equal(pthread._id);
-				thread.title.should.equal(pthread.title);
-				next(err);
-			}
+			should.not.exist(err);
+			thread._id.should.equal(pthread._id);
+			thread.title.should.equal(pthread.title);
+			next(err);
 		});
 	});
 	it('can update record', function (next) {
@@ -256,87 +220,66 @@ describe('thread collection', function () {
 		pthread.hit = 17;
 		mongo.updateThread(pthread);
 		mongo.findThreadById(pthread._id, function (err, thread) {
-			if (err) {
-				next(err);
-			} else {
-				thread.should.eql(pthread);
-				next();
-			}
+			should.not.exist(err);
+			thread.should.eql(pthread);
+			next();
 		});
 	});
 	it('can increase hit', function (next) {
 		mongo.updateThreadHit(pthread);
 		mongo.findThreadById(pthread._id, function (err, thread) {
-			if (err) {
-				next(err);
-			} else {
-				thread.hit.should.equal(pthread.hit + 1);
-				next();
-			}
+			should.not.exist(err);
+			thread.hit.should.equal(pthread.hit + 1);
+			next();
 		});
 	});
 	it('can update lenth & updated', function (next) {
 		var now = new Date();
 		mongo.updateThreadLength(pthread, now);
 		mongo.findThreadById(pthread._id, function (err, thread) {
-			if (err) {
-				next(err);
-			} else {
-				thread.length.should.equal(pthread.length + 1);
-				thread.updated.should.equal(now);
-				next();
-			}
+			should.not.exist(err);
+			thread.length.should.equal(pthread.length + 1);
+			thread.updated.should.equal(now);
+			next();
 		});
 	});
 	describe('findThreadByCategory', function () {
 		it('should success when categoryId is 0', function (next) {
 			mongo.findThreadByCategory(0, 1, 99, function (err, thread) {
-				if (err) {
-					next(err);
-				} else {
-					thread.should.length(10);
-					thread[0].title.should.equal('title10');
-					thread[1].title.should.equal('title9');
-					thread[2].title.should.equal('title8');
-					thread[9].title.should.equal('title1');
-					next();
-				}
+				should.not.exist(err);
+				thread.should.length(10);
+				thread[0].title.should.equal('title10');
+				thread[1].title.should.equal('title9');
+				thread[2].title.should.equal('title8');
+				thread[9].title.should.equal('title1');
+				next();
 			})
 		});
 		it('should success when categoryId is 101', function (next) {
 			mongo.findThreadByCategory(101, 1, 99, function (err, thread) {
-				if (err) {
-					next(err);
-				} else {
-					thread.should.length(4);
-					next(err);
-				}
+				should.not.exist(err);
+				thread.should.length(4);
+				next(err);
 			});
 		});
 		it('should success when page is 2', function (next) {
 			mongo.findThreadByCategory(0, 2, 3, function (err, thread) {
-				if (err) {
-					next(err);
-				} else {
-					thread.should.length(3);
-					thread[0].title.should.equal('title7');
-					thread[1].title.should.equal('title6');
-					thread[2].title.should.equal('title5');
-					next();
-				}
+				should.not.exist(err);
+				thread.should.length(3);
+				thread[0].title.should.equal('title7');
+				thread[1].title.should.equal('title6');
+				thread[2].title.should.equal('title5');
+				next();
 			})
 		});
 		it('should success when page is -1', function (next) {
 			mongo.findThreadByCategory(0, -1, 3, function (err, thread) {
-				if (err) {
-					next(err);
-				} else {
-					thread.should.length(3);
-					thread[0].title.should.equal('title1');
-					thread[1].title.should.equal('title2');
-					thread[2].title.should.equal('title3');
-					next();
-				}
+				should.not.exist(err);
+				thread.should.length(3);
+				thread[0].title.should.equal('title1');
+				thread[1].title.should.equal('title2');
+				thread[2].title.should.equal('title3');
+				next();
 			})
 		});
 	});
