9a0a9eb2694a91a06799c46125b19a8c06c6b963
drypot <drypot@gmail.com>
2013-04-03 22:16:40 +0900

    working express.js.

--- server/main/express.js
+++ server/main/express.js
@@ -1,57 +1,59 @@
 var _ = require('underscore');
 var express = require('express');
 var redisStore = require('connect-redis')(express);
-var dust = require('dustjs-linkedin');
-var consolidate = require('consolidate');
+
+//var dust = require('dustjs-linkedin');
+//var consolidate = require('consolidate');
+
 var l = require('./l.js');
+var config = require('./config.js');
+var role = require('./role.js');
 
-require('./config.js');
-require('./role.js');
+exports.init = function () {
 
-l.init(function () {
+	var app = express();
+	var roleByName = role.roleByName;
 
-	var e = l.e = express();
+	app.configure(function () {
+		app.use(express.cookieParser(config.cookieSecret));
+		app.use(express.session({ store: new redisStore() }));
+		app.use(express.bodyParser({ uploadDir: config.uploadDir + '/tmp' }));
 
-	e.configure(function () {
-		e.use(express.cookieParser(l.config.cookieSecret));
-		e.use(express.session({ store: new redisStore() }));
-		e.use(express.bodyParser({ uploadDir: l.config.uploadDir + '/tmp' }));
-
-		e.use(function (req, res, next) {
-			res.locals.role = l.role.roleByName(req.session.roleName);
+		app.use(function (req, res, next) {
+			res.locals.role = roleByName(req.session.roleName);
 			next();
 		});
 
-		e.use(e.router);
+		app.use(app.router);
 
 		// NODE_ENV=production 상태에서는 케쉬 때문에 *.dust 파일 수정해도 반영이 안 된다.
-		e.engine('dust', consolidate.dust); // 확장자별 뷰 처리 엔진 등록
-		e.set('view engine', 'dust'); // 뷰 기본 확장자 등록
-		e.set('views', process.cwd() + '/client/dust'); // 뷰 루트 디렉토리 등록
+		app.engine('dust', consolidate.dust); // 확장자별 뷰 처리 엔진 등록
+		app.set('view engine', 'dust'); // 뷰 기본 확장자 등록
+		app.set('views', process.cwd() + '/client/dust'); // 뷰 루트 디렉토리 등록
 
 		// To disable whitespace compression
 		//dust.optimizers.format = function(ctx, node) { return node };
 
-		e.locals.siteTitle = l.config.siteTitle;
+		app.locals.siteTitle = config.siteTitle;
 		//e.locals._ = _;
 		//e.locals._with = false; // for ejs
 
-		e.all('/api/*', function (req, res, next) {
+		app.all('/api/*', function (req, res, next) {
 			// to solve IE ajax caching problem.
 			res.set('Cache-Control', 'no-cache');
 			next();
 		});
 	});
 
-	e.configure('development', function () {
-		e.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	app.configure('development', function () {
+		app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 	});
 
-	e.configure('production', function () {
-		e.use(express.errorHandler());
+	app.configure('production', function () {
+		app.use(express.errorHandler());
 	});
 
-	e.get('/api/hello', function (req, res) {
+	app.get('/api/hello', function (req, res) {
 		res.json('hello');
 	});
 
@@ -78,13 +80,13 @@ l.init(function () {
 			}
 		}
 	}
-});
+};
 
 l.init(1, function () {
 
-	if (!l.config.expressDisabled) {
-		l.e.listen(l.config.serverPort);
-		console.log("express listening on port: %d", l.config.serverPort);
+	if (!config.expressDisabled) {
+		l.e.listen(config.serverPort);
+		console.log("express listening on port: %d", config.serverPort);
 	}
 
 });
