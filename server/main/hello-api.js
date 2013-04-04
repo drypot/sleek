589067b7a47795a05fb45ac488597f2866597d28
drypot <drypot@gmail.com>
2013-04-05 03:10:46 +0900

    express-test.js.

--- server/main/hello-api.js
+++ server/main/hello-api.js
@@ -1,7 +1,10 @@
 var express = require('./express');
 
-exports.init = function() {
+exports.init = function(next) {
+
 	express.app.get('/api/hello', function (req, res) {
 		res.json('hello');
 	});
+
+	next();
 };
--- server/test/express-test.js
+++ server/test/express-test.js
@@ -1,22 +1,40 @@
 var should = require('should');
+var request = require('superagent').agent();
 
 var config = require('../main/config');
 var role = require('../main/role');
 var express = require('../main/express');
-
-require('../main/express');
-require('../main/test');
+var hello = require('../main/hello-api');
 
 before(function (next) {
-	l.init.run(next);
+	config.init({ test: true }, next);
 });
 
-describe('get hello', function () {
+before(function (next) {
+	role.init(next);
+});
+
+before(function (next) {
+	express.init(next);
+});
+
+before(function (next) {
+	hello.init(next);
+});
+
+var url;
+
+before(function () {
+	url = 'http://localhost:' + config.port;
+	express.app.listen(config.port);
+});
+
+describe('hello', function () {
 	it('should return hello', function (next) {
-		l.test.request.get('/api/hello', function (err, res) {
-			res.status.should.equal(200);
+		request.get(url + '/api/hello', function (err, res) {
+			res.should.status(200);
 			res.body.should.equal('hello');
-			next(err);
+			next();
 		});
 	});
 });
