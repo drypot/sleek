863a427600ce445cb0bba05395e1cb08ff9ac5c5
drypot <drypot@gmail.com>
2013-04-06 00:38:18 +0900

    functional config.js.

--- server/main/config.js
+++ server/main/config.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-exports.init = function (opt) {
+exports = module.exports = function (opt, next) {
 	if (opt.test) {
 		opt.path = 'config/config-test.json';
 	}
@@ -8,10 +8,7 @@ exports.init = function (opt) {
 		console.error('specify configuration path.')
 		process.exit();
 	}
-	var text = fs.readFileSync(opt.path, 'utf8');
-	var config = JSON.parse(text);
-	for (var p in config) {
-		exports[p] = config[p];
-	}
 	console.log('config file: ' + opt.path);
+	var text = fs.readFileSync(opt.path, 'utf8');
+	next(JSON.parse(text));
 }
--- server/test/config-test.js
+++ server/test/config-test.js
@@ -1,32 +1,27 @@
 var should = require('should');
 
-var l = require('../main/l');
 var config = require('../main/config');
 
 describe('config with invalid path', function () {
 	it('should throw', function () {
 		(function () {
-			config.init({ path: 'config/config-none.json' });
+			config({ path: 'config/config-none.json' }, function (config) {});
 		}).should.throw();
 	});
 });
 
 describe('config with test: true', function () {
 	it('should success', function () {
-		(function () {
-			config.init({ test: true });
-		}).should.not.throw();
+		config({ test: true }, function (config) {
+			config.siteTitle.should.equal("sleek test");
+		});
 	});
 });
 
 describe('config with valid path', function () {
 	it('should success', function () {
-		(function () {
-			config.init({ path: 'config/config-test.json' });
-		}).should.not.throw();
-	});
-	it('should have siteTitle', function () {
-		config.siteTitle.should.equal("sleek test");
+		config({ path: 'config/config-test.json' }, function (config) {
+			config.siteTitle.should.equal("sleek test");
+		});
 	});
 });
-
