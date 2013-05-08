0283e25ef09f03e8ec355c88f4171139b966f02b
drypot <drypot@gmail.com>
2013-05-08 23:29:52 +0900

    auto-login refined.

--- client/public/js/session.js
+++ client/public/js/session.js
@@ -3,16 +3,36 @@ init.add(function () {
 
 	window.session = {};
 
-	session.initLoginPage = function () {
+	var $loginSec;
+
+	session.initLogin = function () {
 		trySavedPassword(function (err, success) {
-			if (err) return msgBox.error(err);
+			if (err) return showError.system(err);
 			if (success) {
 				location = '/threads';
 				return;
 			}
-			$content.find('[name=submit]').click(sendLoginForm);
+			$loginSec = $('#login-sec');
+			$loginSec.removeClass('hide');
+			$loginSec.find('[name=submit]').click(sendLoginForm);
 		});
 	};
+
+	session.initAutoLogin = function () {
+		trySavedPassword(function (err, success) {
+			if (err) {
+				showError.system(err, function () {
+					location = '/';
+				});
+				return
+			}
+			if (success) {
+				location.reload();
+				return;
+			}
+			location = '/';
+		});
+	}
 
 	function trySavedPassword(next) {
 		var pw = localStorage.getItem('password');
@@ -20,18 +40,21 @@ init.add(function () {
 		console.log('trying saved password,');
 		request.post('/api/sessions').send({ password: pw }).end(function (err, res) {
 			err = err || res.error || res.body.err;
-			if (err) return next(err, false);
+			if (err) {
+				localStorage.removeItem('password');
+				return next(err, false);
+			}
 			next(null, true);
 		});
 	}
 
 	function sendLoginForm() {
-		var $password = $content.find('[name=password]');
-		var $remember = $content.find('[name=remember]');
-		alerts.clear($content);
+		var $password = $loginSec.find('[name=password]');
+		var $remember = $loginSec.find('[name=remember]');
+		alerts.clear($loginSec);
 		request.post('/api/sessions').send({ password: $password.val() }).end(function (err, res) {
 			err = err || res.error;
-			if (err) return msgBox.error(err);
+			if (err) return showError.system(err);
 			if (res.body.err) {
 				alerts.add($password, res.body.err.message);
 				return;
@@ -49,7 +72,7 @@ init.add(function () {
 	session.logout = function () {
 		request.del('/api/sessions').end(function (err, res) {
 			err = err || res.error || res.body.err;
-			if (err) return msgBox.error(err);
+			if (err) return showError.system(err);
 			localStorage.removeItem('password');
 			console.log('logged out');
 			location = '/';
