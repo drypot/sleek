8623cfc20e363a06e5ef1832ee1c97be3bbd6aba
drypot <drypot@gmail.com>
2013-05-24 05:16:14 +0900

    removed spinner.

--- client/public/js/ui.js
+++ client/public/js/ui.js
@@ -236,24 +236,3 @@ init.add(function () {
 //		$('.navbar input[name="q"]').val(url.query.q);
 //	}
 });
-
-init.add(function () {
-
-	window.spinner = new Spinner({
-		lines: 11, // The number of lines to draw
-		length: 5, // The length of each line
-		width: 2, // The line thickness
-		radius: 6, // The radius of the inner circle
-		rotate: 0, // The rotation offset
-		color: '#000', // #rgb or #rrggbb
-		speed: 1, // Rounds per second
-		trail: 60, // Afterglow percentage
-		shadow: false, // Whether to render a shadow
-		hwaccel: false, // Whether to use hardware acceleration
-		className: 'spinner', // The CSS class to assign to the spinner
-		zIndex: 2e9, // The z-index (defaults to 2000000000)
-		top: 'auto', // Top position relative to parent in px
-		left: 'auto' // Left position relative to parent in px
-	});
-
-});
