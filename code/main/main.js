a78831e567280490e2b3829e778c2657f30301c7
drypot <drypot@gmail.com>
2021-02-24 00:37:24 +0900

    catch SIGINT.

--- code/main/main.js
+++ code/main/main.js
@@ -3,6 +3,7 @@
 const init = require('../base/init');
 const config = require('../base/config');
 const expb = require('../express/express-base');
+const mysql2 = require('../mysql/mysql2');
 
 require('../user/user-base');
 require('../post/post-list');
@@ -11,9 +12,17 @@ require('../post/post-new');
 require('../post/post-update');
 require('../post/post-search');
 
+process.on('SIGINT', function() {
+  mysql2.close(function(err) {
+    console.log("SIGINT caught");
+    process.exit(err ? 1 : 0);
+  });
+});
+
 init.add((done) => {
   expb.start();
   done();
 });
 
 init.run();
+
