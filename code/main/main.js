60e79a5990e9b7dffa4efcc637a9c78e78bbd55e
drypot <drypot@gmail.com>
2021-03-16 01:17:56 +0900

    fix main.js.

--- code/main/main.js
+++ code/main/main.js
@@ -4,12 +4,12 @@ import * as config from '../base/config.js';
 import * as expb from '../express/express-base.js';
 import * as db from '../db/db.js';
 
-import "../post/post-search";
-import "../post/post-update";
-import "../post/post-new";
-import "../post/post-view";
-import "../post/post-list";
-import "../user/user-base";
+import "../post/post-search.js";
+import "../post/post-update.js";
+import "../post/post-new.js";
+import "../post/post-view.js";
+import "../post/post-list.js";
+import "../user/user-base.js";
 
 process.on('SIGINT', function() {
   db.close(function(err) {
